d3d3a15ead1d4abf5c0834376a1abddebf890077
Stefano Verna <s.verna@cantierecreativo.net>
2014-08-26 18:52:31 +0200

    Console working

--- test.js
+++ test.js
@@ -1,42 +1,42 @@
-var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({port: 9333});
+var open = require('open');
+var WsServer = require('ws').Server;
+
+var wss = new WsServer({ port: 9333 });
 
 wss.on('connection', function(ws) {
-  console.log("CIAO");
   ws.on('message', function(message) {
-    console.log('received: %s', message);
+    message = JSON.parse(message);
+
+    if (message.method == 'Console.enable') {
+      ws.send(JSON.stringify({
+        id: message.id,
+        result: {}
+      }));
+      ws.send(JSON.stringify({
+        "method":"Console.messageAdded",
+        "params": {
+          "message": {
+            "source": "console-api",
+            "level": "log",
+            "text": "BELLA DUE",
+            "timestamp": new Date().getTime(),
+            "type": "log",
+            "parameters": [{"type":"string","value":"BELLA DUE"}],
+            "stackTrace": []
+          }
+        }
+      }));
+    } else {
+      ws.send(JSON.stringify({
+        id: message.id,
+        error: {
+          code:-32601,
+          message: "'" + message.method + "' wasn't found"
+        }
+      }));
+    }
   });
-
-  var contextCreated = {
-    "method":"Runtime.executionContextCreated",
-    "params": {
-      "context": {
-        "id":48,
-        "isPageContext":true,
-        "frameId":"7907.24"
-      }
-    }
-  };
-
-  var messageAdded = {
-    "method":"Console.messageAdded",
-    "params": {
-      "message": {
-        "source":"console-api",
-        "level":"log",
-        "text":"BELLA DUE",
-        "timestamp":new Date().getTime(),
-        "type":"log",
-        "parameters":[{"type":"string","value":"BELLA DUE"}],
-        "stackTrace":[]
-      }
-    }
-  };
-
-  setTimeout(function() { 
-    ws.send(JSON.stringify(contextCreated)); 
-    ws.send(JSON.stringify(messageAdded)); 
-    console.log("Sent"); 
-  }, 5000);
 });
+
+// open('http://127.0.0.1:8000/front_end/inspector.html?ws=127.0.0.1:9333');
 
